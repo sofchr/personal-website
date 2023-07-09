95ca367d370d5534a29a9d3255ba36c29f610d65
Sofie Christensen <sofiec510@gmail.com>
2023-07-08 22:03:01 -0400

    add final dev styling updates

---
+++ index.js
@@ -0,0 +1,11 @@
+const name = 'Sofie Christensen';
+
+const message = `Welcome to the website of ${name}`;
+
+console.log(message);
+
+function showAlert() {
+    window.alert(message);
+   
+   }
+
